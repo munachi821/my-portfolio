8e678d3c65ed8d1d351e1f718f5c95e941a79304
Munachi <munaonye4@gmail.com>
2026-07-14 23:28:06 +0100

    added the favicons

--- app/layout.tsx
+++ app/layout.tsx
@@ -65,6 +65,17 @@ export const metadata: Metadata = {
       follow: true,
     },
   },
+  icons: {
+    icon: [
+      { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+      { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
+    ],
+    apple: [{ url: "/apple-touch-icon.png", sizes: "180x180", type: "image/png" }],
+    other: [
+      { rel: "icon", type: "image/png", sizes: "192x192", url: "/android-chrome-192x192.png" },
+      { rel: "icon", type: "image/png", sizes: "512x512", url: "/android-chrome-512x512.png" },
+    ],
+  },
 };
 
 export default function RootLayout({
